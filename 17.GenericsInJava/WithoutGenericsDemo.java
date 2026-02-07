16047711de5af7c0eeec8a1c76232ce69cc9d1d4
RAJANNITCS <rajan.nit.cs@gmail.com>
2026-02-07 22:33:39 +0530

    add generice

--- 17.GenericsInJava/WithoutGenericsDemo.java
+++ 17.GenericsInJava/WithoutGenericsDemo.java
@@ -1,18 +1,18 @@
 // WAP to demostrating a class without generics
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 public class WithoutGenericsDemo {
     public static void main(String[] args) {
-        ArrayList list = new ArrayList();
-        list.add("hello");
-        list.add(123);
-        list.add(3.14);
+        // ArrayList list = new ArrayList();
+        // list.add("hello");
+        // list.add(123);
+        // list.add(3.14);
 
-        String str = (String) list.get(0);
-        String str1 = (String) list.get(1);
+        // String str = (String) list.get(0);
+        // String str1 = (String) list.get(1);
 
-        System.out.println(str);
-        System.out.println(str1);
+        // System.out.println(str);
+        // System.out.println(str1);
     }
 }
